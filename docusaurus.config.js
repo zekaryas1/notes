5ae67577fb1f41142ada227416ae69c7ac31e5f1
zekaryas1 <zekaryasdinku@gmail.com>
2022-06-30 21:20:53 +0300

    changed config files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: "Zack's note and blogs",
   tagline: 'Brief notes here and there',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://zekaryas1.github.io',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'zekaryas1', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'brief-programming-notes', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
