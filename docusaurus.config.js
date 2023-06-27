4bbd12b1ca9045deec0dd19ca18fd5db789c3b1b
zekaryas <zekaryasdinku@gmail.com>
2023-06-27 12:36:00 +0300

    SEO keywords and descriptions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,54 +1,52 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/vsLight');
-const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
+const lightCodeTheme = require("prism-react-renderer/themes/vsLight");
+const darkCodeTheme = require("prism-react-renderer/themes/vsDark");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Zack's note and blogs",
-  tagline: 'Brief notes here and there',
-  url: 'https://zack.onrender.com',
-  baseUrl: '/',
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  tagline: "Brief notes here and there",
+  url: "https://zack.onrender.com",
+  baseUrl: "/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
   trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'zekaryas1', // Usually your GitHub org/user name.
-  projectName: 'notes', // Usually your repo name.
+  organizationName: "zekaryas1", // Usually your GitHub org/user name.
+  projectName: "notes", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/zekaryas1/notes/blob/main/',
+          editUrl: "https://github.com/zekaryas1/notes/blob/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/zekaryas1/notes/blob/main/',
+          editUrl: "https://github.com/zekaryas1/notes/blob/main/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -64,77 +62,79 @@ const config = {
         },
       },
       announcementBar: {
-        id: 'support_us',
-        content: '⭐️ If this website can help you, give a star to support the author on <a target="_blank" rel="noopener noreferrer" href="https://github.com/zekaryas1/notes">GitHub</a>',
-        backgroundColor: '#fafbfc',
-        textColor: '#091E42',
+        id: "support_us",
+        content:
+          '⭐️ If this website can help you, give a star to support the author on <a target="_blank" rel="noopener noreferrer" href="https://github.com/zekaryas1/notes">GitHub</a>',
+        backgroundColor: "#fafbfc",
+        textColor: "#091E42",
         isCloseable: true,
-      }, 
+      },
       navbar: {
         title: "Zack's",
         hideOnScroll: true,
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "My Site Logo",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'search',
-            position: 'right',
+            type: "search",
+            position: "right",
           },
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Note',
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Note",
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          { to: "/resume", label: "Resume", position: "left" },
+          { to: "/blog", label: "Blog", position: "left" },
           {
-            href: 'https://github.com/zekaryas1/notes',
-            label: 'GitHub',
-            position: 'left',
+            href: "https://github.com/zekaryas1/notes",
+            label: "GitHub",
+            position: "left",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Notes on',
+            title: "Notes on",
             items: [
               {
-                label: 'Algorithm & Data structure',
-                to: '/docs/Algo/Fundamental Algorithms/Sorting algos/Radix Sort',
+                label: "Algorithm & Data structure",
+                to: "/docs/Algo/Fundamental Algorithms/Sorting algos/Radix Sort",
               },
               {
-                label: 'Python tips and Tricks',
-                to: '/docs/Algo/Tips & Tricks/Useful Python Collections module',
+                label: "Python tips and Tricks",
+                to: "/docs/Algo/Tips & Tricks/Useful Python Collections module",
               },
             ],
           },
           {
-            title: 'More Notes on',
+            title: "More Notes on",
             items: [
               {
-                label: 'Docker & Docker compose',
-                to: '/docs/Containerization/Docker/General Step to Dockerizing a Project',
+                label: "Docker & Docker compose",
+                to: "/docs/Containerization/Docker/General Step to Dockerizing a Project",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: "Blog",
+                to: "/blog",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/zekaryas1/notes',
+                label: "GitHub",
+                href: "https://github.com/zekaryas1/notes",
               },
               {
-                label: 'Contact me on Linkedin',
-                href: 'https://www.linkedin.com/in/zekaryas-tadele-dinku',
+                label: "Contact me on Linkedin",
+                href: "https://www.linkedin.com/in/zekaryas-tadele-dinku",
               },
             ],
           },
@@ -144,16 +144,26 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['bash'],
+        additionalLanguages: ["bash"],
       },
       algolia: {
         // The application ID provided by Algolia
-        appId: '4QUF041B88',
+        appId: "4QUF041B88",
         // Public API key: it is safe to commit it
-        apiKey: '160f52becaea805c46780c776893836e',
-        indexName: 'zack-onrender',
-        debug: false
-      }
+        apiKey: "160f52becaea805c46780c776893836e",
+        indexName: "zack-onrender",
+        debug: false,
+      },
+      metadata: [
+        {
+          name: "keywords",
+          content: `Programming language notes, Docker notes, TypeScript notes, Algorithm and data structure notes, Technical notes for programmers, Programming tips and tricks, Coding best practices, Beginner-friendly programming resources, Learning resources for new programmers, Programming tutorials, Code snippets and examples, Software engineering insights`,
+        },
+        {
+          name: "description",
+          content: `Welcome to my website, where I share my technical notes on software engineering, coding, and programming languages. You'll also find my resume, which highlights my experience and side projects. Whether you're interested in web development, front-end development, or back-end development, you're sure to find valuable insights and information here.`,
+        },
+      ],
     }),
 };
 
