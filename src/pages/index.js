1fdeba62af51d9c3e4401f1c0b7d85e3c9908e5a
Zekaryas Tadele <zekarasdinku@gmail.com>
2024-12-30 13:01:47 +0300

    - adds required alt to img

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ function HomepageHeader() {
   return (
     <header className={clsx(styles.heroBanner)}>
       <div className="container">
-        <img src="img/logo.svg" style={{width: 280, borderRadius: '50%'}} /> 
+        <img src="img/logo.svg" alt={"website mascot"} style={{width: 280, borderRadius: '50%'}} />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
