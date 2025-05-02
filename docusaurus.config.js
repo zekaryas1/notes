a4b99e87a01b8c79e26b35f47e0ca926068047e6
Zekaryas Tadele <zekarasdinku@gmail.com>
2025-05-02 15:20:18 +0300

    moves offline docs to private

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,7 +33,6 @@ const config = {
             /** @type {import('@docusaurus/preset-classic').Options} */
             ({
                 docs: {
-                    exclude: ["**/Algo/Coding Practice/**", "**/System design/**"],
                     sidebarPath: require.resolve("./sidebars.js"),
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
